179eed472c7711f678e5d3818eab9a445ffca419
laggiesfl <167820181+laggiesfl@users.noreply.github.com>
2026-09-09 23:57:16 +0200

    Keep one prominent desktop app link

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -39,7 +39,6 @@ export default function Nav() {
           <li><Link href="/#membership">Join</Link></li>
           <li><Link href="/#donate">Donate</Link></li>
           <li><Link href="/#contact">Contact</Link></li>
-          <li><Link href="/app-home">Open Voice of Disability App</Link></li>
         </ul>
 
         <Link href="/app-home" className="btn btn-primary nav-cta">
